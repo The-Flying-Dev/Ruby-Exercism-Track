084143834f65ec82b0e1fbdfca335160e2b177f0
The-Flying-Dev <leandranm@outlook.com>
2023-05-29 13:18:52 +0200

    log line parser

--- exercises/concept/log-line-parser/log_line_parser.rb
+++ exercises/concept/log-line-parser/log_line_parser.rb
@@ -1,17 +1,25 @@
 class LogLineParser
   def initialize(line)
     @line = line
+    @log_message = @line.split(": ")[1].strip
+    @level = @line.split(':')[0].gsub(/[\[\]]/, '').downcase
   end
 
   def message
-    raise 'Please implement the LogLineParser#message method'
+    #raise 'Please implement the LogLineParser#message method'    
+    @log_message
   end
+   
+
 
   def log_level
-    raise 'Please implement the LogLineParser#log_level method'
+    #raise 'Please implement the LogLineParser#log_level method'    
+    @level
   end
 
   def reformat
-    raise 'Please implement the LogLineParser#reformat method'
+    #raise 'Please implement the LogLineParser#reformat method'
+    "#{message} (#{log_level})"
   end
+  
 end
