d8789a0d317cd8d9b56705a818396b8b03b9d47e
The-Flying-Dev <leandranm@outlook.com>
2023-05-29 15:25:45 +0200

    assembly line

--- exercises/concept/assembly-line/assembly_line.rb
+++ exercises/concept/assembly-line/assembly_line.rb
@@ -3,11 +3,25 @@ class AssemblyLine
     @speed = speed
   end
 
-  def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+  def production_rate_per_hour   
+    
+    if @speed.between?(1, 4)
+      result = @speed.to_f * 221      
+      result
+    elsif @speed.between?(5, 8)
+      result = (@speed.to_f * 221 * 90) / 100       
+      result
+    elsif @speed == 9
+      result = (@speed.to_f * 221 * 80) / 100       
+      result
+    else  
+      result = (@speed.to_f * 221 * 77) / 100
+      result
+    end
   end
 
-  def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+  def working_items_per_minute  
+    production_rate_per_hour.to_i / 60
   end
+
 end
