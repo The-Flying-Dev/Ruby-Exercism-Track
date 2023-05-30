e3446c2977697b923b8215a9ceed1231918d5b0c
The-Flying-Dev <leandranm@outlook.com>
2023-05-30 18:51:51 +0200

    savings account

--- exercises/concept/savings-account/savings_account.rb
+++ exercises/concept/savings-account/savings_account.rb
@@ -1,13 +1,29 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    
+    case balance    
+    when (0...1000) then 0.5
+    when (1000...5000) then 1.621
+    when (5000..) then 2.475  
+    else 3.213     
+    end
+    
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    interest_accumulated = (self.interest_rate(balance) * balance) / 100
+    interest_accumulated + balance    
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+     
+    counter = 0
+    while current_balance < desired_balance
+      current_balance = annual_balance_update(current_balance)
+      counter += 1  
+    end
+    counter
+     
   end
+    
 end
